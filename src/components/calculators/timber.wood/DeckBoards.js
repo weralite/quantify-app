6df2c9311eb8b07a3a5fc9decf303073555b6b67
mazmann <sadi.lundberg@hotmail.com>
2024-06-02 16:58:21 +0200

    added pickerfields for test

--- src/components/calculators/timber.wood/DeckBoards.js
+++ src/components/calculators/timber.wood/DeckBoards.js
@@ -7,7 +7,9 @@ import InputField from '../../inputs/InputField'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const DeckBoards = () => {
+  const [selectedThickness, setSelectedThickness] = React.useState('28');
   const [selectedWidth, setSelectedWidth] = React.useState('120');
+  const [selectedLength, setSelectedLength] = React.useState('4200');
   const [area, setArea] = React.useState('');
   const [result, setResult] = React.useState(0);
   const [increasedResult, setIncreasedResult] = React.useState(0);
@@ -42,9 +44,9 @@ const DeckBoards = () => {
 
   const saveResultsToNotes = async () => {
     const notes = await AsyncStorage.getItem('notes');
-    const newNotes = notes ? `${notes}\n
-    ${increasedResult} LPM (inkl 10%), ${selectedWidth}mm` : `
-    ${increasedResult} LPM (inkl 10%), ${selectedWidth}mm`;
+    const newNotes = notes
+      ? `${notes}\n\nTrall\n${increasedResult} LPM (inkl 10%), ${selectedWidth}mm`
+      : `Trall\n${increasedResult} LPM (inkl 10%), ${selectedWidth}mm`;
     await AsyncStorage.setItem('notes', newNotes);
   };
 
@@ -53,11 +55,40 @@ const DeckBoards = () => {
 
       <View>
         <Text style={[styles.sectionHeader]}>Trall</Text>
-        <Text style={[styles.sectionContent]}>Ange ytan som ska täckas och virkets bredd.</Text>
+        <Text style={[styles.sectionContent]}>Ange ytan som ska täckas och virkets dimension.</Text>
       </View>
 
+      <View style={[styles.inputRowOne]}>
 
-      <View style={[styles.inputContainer]}>
+        <View style={{ height: 40, justifyContent: 'center', borderWidth: 1, borderColor: 'gray' }}>
+          <Picker
+            selectedValue={selectedThickness}
+            style={{ width: screenWidth * 0.30 }}
+            onValueChange={handleSelectedWidthChange}>
+
+            <Picker.Item label="22" value="22" />
+            <Picker.Item label="28" value="28" />
+            <Picker.Item label="34" value="34" />
+
+          </Picker>
+        </View>
+
+        <View style={{ height: 40, justifyContent: 'center', borderWidth: 1, borderColor: 'gray' }}>
+          <Picker
+            selectedValue={selectedWidth}
+            style={{ width: screenWidth * 0.36 }}
+            onValueChange={handleSelectedWidthChange}>
+
+            <Picker.Item label="95" value="95" />
+            <Picker.Item label="120" value="120" />
+            <Picker.Item label="145" value="145" />
+
+          </Picker>
+        </View>
+
+      </View>
+
+      <View style={[styles.inputRowTwo]}>
 
         <InputField
           style={{ width: screenWidth * 0.20 }}
@@ -70,13 +101,20 @@ const DeckBoards = () => {
 
         <View style={{ height: 40, justifyContent: 'center', borderWidth: 1, borderColor: 'gray' }}>
           <Picker
-            selectedValue={selectedWidth}
+            selectedValue={selectedLength}
             style={{ width: screenWidth * 0.45 }}
             onValueChange={handleSelectedWidthChange}>
 
-            <Picker.Item label="95mm" value="95" />
-            <Picker.Item label="120mm" value="120" />
-            <Picker.Item label="145mm" value="145" />
+            <Picker.Item label="3000" value="3000" />
+            <Picker.Item label="3300" value="3300" />
+            <Picker.Item label="3600" value="3600" />
+            <Picker.Item label="3900" value="3900" />
+            <Picker.Item label="4200" value="4200" />
+            <Picker.Item label="4500" value="4500" />
+            <Picker.Item label="4800" value="4800" />
+            <Picker.Item label="5100" value="5100" />
+            <Picker.Item label="5400" value="5400" />
+  
 
           </Picker>
         </View>
@@ -89,8 +127,8 @@ const DeckBoards = () => {
         <View style={{ flexDirection: 'column', alignItems: 'center' }}>
           <Text>Du behöver {increasedResult} LPM inkl 10% marginal, ({result} LPM exkl marginal)</Text>
           <View style={[styles.buttonContainer]}>
-          <Button title="Spara" onPress={saveResultsToNotes} />
-          <Button title="Avbryt" onPress={handleReset} />
+            <Button title="Spara" onPress={saveResultsToNotes} />
+            <Button title="Avbryt" onPress={handleReset} />
           </View>
         </View>
       )}
@@ -117,7 +155,15 @@ const styles = StyleSheet.create({
     color: '#000',
     marginBottom: 10,
   },
-  inputContainer: {
+  inputRowOne: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginBottom: 10,
+    gap: 9,
+  },
+  inputRowTwo: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -126,7 +172,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     display: 'flex',
     flexDirection: 'row',
-gap: 10,
+    gap: 10,
     alignItems: 'center',
     margin: 10,
   },
