7f96dcd98e2c454e1ca91cc4264446ae95400726
mazmann <sadi.lundberg@hotmail.com>
2024-06-24 21:01:29 +0200

    added some temporary error glow

--- src/components/common/calculatorLayoutComponents/DropdownSelectCell.js
+++ src/components/common/calculatorLayoutComponents/DropdownSelectCell.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
 
-const DropdownSelectCell = ({ children, style, error }) => (
-  <View style={[styles.dropdownSelectCell, error && styles.errorStyle, style]}>
-    {children}
-  </View>
-);
+const DropdownSelectCell = ({ children, style, error }) => {
+  const borderColorAnim = useRef(new Animated.Value(0)).current; // Step 2
+
+  useEffect(() => { // Step 3
+    if (error) {
+      Animated.loop( // Loop the animation
+        Animated.sequence([ // Sequence of animations
+          Animated.timing(borderColorAnim, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: false,
+          }),
+          Animated.timing(borderColorAnim, {
+            toValue: 0,
+            duration: 1000,
+            useNativeDriver: false,
+          }),
+        ]),
+      ).start();
+    }
+  }, [error, borderColorAnim]);
+
+  const animatedBorderStyle = {
+    borderColor: borderColorAnim.interpolate({ // Step 4
+      inputRange: [0, 1],
+      outputRange: ['red', '#FFA07A'], // Change colors as needed
+    }),
+  };
+
+  return (
+    <Animated.View // Step 5
+      style={[
+        styles.dropdownSelectCell,
+        error && animatedBorderStyle,
+        style,
+      ]}>
+      {children}
+    </Animated.View>
+  );
+};
 
 export default DropdownSelectCell;
 
@@ -17,8 +52,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderWidth: 1,
     borderColor: 'gray',
-  },
-  errorStyle: {
-    borderColor: 'red', // Example error style
   },
 });
